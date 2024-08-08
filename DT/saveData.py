bad5c810cd5bec5b5dd91dd4e0900e375a7db79d
dasihg <62131361+dasihg@users.noreply.github.com>
2024-08-08 11:40:54 +0800

    Add files via upload
    DT：使用 Python 构建简单决策树的示例代码，使用了 scikit-learn 库
    GNB：使用scikit-learn库中的朴素贝叶斯算法（以高斯朴素贝叶斯为例）对鸢尾花数据集进行分类的 Python 示例

---
+++ DT/saveData.py
@@ -0,0 +1,12 @@
+﻿from sklearn.datasets import load_iris
+import pandas as pd
+
+# 加载鸢尾花数据集
+iris = load_iris()
+
+# 将数据转换为 DataFrame
+iris_df = pd.DataFrame(iris.data, columns=iris.feature_names)
+iris_df['target'] = iris.target
+
+# 将 DataFrame 保存为 CSV 文件
+iris_df.to_csv('iris_data.csv', index=False)
---
+++ GNB/gnb.py
@@ -0,0 +1,44 @@
+﻿from sklearn import tree
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
+
+# 加载鸢尾花数据集
+# iris = load_iris()
+# X = iris.data
+# y = iris.target
+
+# 加载 CSV 文件
+data = pd.read_csv('iris_data.csv')
+# 提取特征和目标变量
+X = data.drop('target', axis=1)
+y = data['target']
+
+# 划分训练集和测试集
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# 创建高斯朴素贝叶斯分类器
+gnb = GaussianNB()
+
+# 在训练集上训练模型
+gnb.fit(X_train, y_train)
+
+# 在测试集上进行预测
+y_pred = gnb.predict(X_test)
+
+# 计算模型准确率
+accuracy = accuracy_score(y_test, y_pred)
+print("模型准确率:", accuracy)
+
+# 将测试集特征和预测结果合并为一个 DataFrame
+test_result_df = pd.DataFrame(X_test)
+test_result_df['Actual Target'] = y_test
+test_result_df['Predicted Target'] = y_pred
+
+# 将结果保存到新的 CSV 文件
+test_result_df.to_csv('test_result.csv', index=True)
+
+# 也可以在控制台打印测试集特征和预测结果
+print(test_result_df)
